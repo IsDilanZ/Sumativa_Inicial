9209b879c1ab7b2f6c45cc8a3e85d89c93d93349
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2024-06-05 11:39:54 -0500

    2nd commit

--- src/main/java/com/example/sumativa_inicial/HelloApplication.java
+++ src/main/java/com/example/sumativa_inicial/HelloApplication.java
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
-
+        //Caizapasto VIllarreal Dilan Ismael
         for (int i=1 ; i <= 101 ; i++ ){
             if (i%3==0 && i%5==0) {
                 System.out.println("fizzbuzz");
